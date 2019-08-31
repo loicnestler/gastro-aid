c88b7551d192c2d3505d4870840cca628b21a35a
loicnestler <loicnestler@gmail.com>
2019-08-31 15:53:18 +0200

    [+] Added CORS headers

--- api/index.js
+++ api/index.js
@@ -19,6 +19,13 @@ app.use(require('koa-respond')())
 app.use(require('koa-logger')())
 app.use(require('koa-helmet')())
 
+app.use(async (ctx, next) => {
+	ctx.set('Access-Control-Allow-Origin', '*')
+	ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+	await next()
+})
+
 app.use(user.middleware())
 generator.addJoiRouter(user)
 
